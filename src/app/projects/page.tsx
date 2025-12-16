6131dde9232accc1f1a4d7c0c36fd9e10482dd8f
Marco Singhof <singhofmarco@gmail.com>
2025-12-16 15:43:29 -0500

    Make project logo's slightly bigger

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -88,7 +88,7 @@ export default function Projects() {
         {projects.map((project) => (
           <Card as="li" key={project.name}>
             <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-md bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image src={project.logo} alt="" className="h-8 w-8" />
+              <Image src={project.logo} alt="" className="h-9 w-9" />
             </div>
             <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
               <Card.Link href={project.link.href} target="_blank">
